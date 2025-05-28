ee4663b7f8e24fc9391dc43641070614192da13c
Anastasia Fedulova <fedulova.a.m@bk.ru>
2025-05-28 14:35:38 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
+'use strict';
+
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		console.log(`${this.name} отчислен.`);
+		return;
+	}
+	if (!!marksToAdd.length) {
+		this.marks.push(...marksToAdd);
+	}
+	return;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	const totalScore = this.marks.reduce((acc, score) => acc + score, 0);
+	return totalScore / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
